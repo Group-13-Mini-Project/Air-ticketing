85ccf1068f24758a52ada6ece9de59eda00e586f
Joseph Ampadu <josephampadu549@gmail.com>
2022-05-08 18:58:17 +0100

    Upgrade expo sdk to 45. Bug fixes (#4)

--- App.jsx
+++ App.jsx
@@ -1,41 +1,26 @@
-import {
-  Syne_400Regular,
-  Syne_500Medium,
-  Syne_600SemiBold,
-  Syne_700Bold,
-  useFonts,
-} from "@expo-google-fonts/syne";
-import AppLoading from "expo-app-loading";
 import { StatusBar } from "expo-status-bar";
+import { LogBox } from "react-native";
 import "react-native-gesture-handler";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { useCachedResources } from "./hooks";
 import { Navigation } from "./navigation";
 
+LogBox.ignoreLogs([
+  "[react-native-gesture-handler] Seems like you're using an old API with gesture components, check out new Gestures system!",
+]);
+
 const App = () => {
-  let [fontsLoaded] = useFonts({
-    Syne_400Regular,
-    Syne_500Medium,
-    Syne_600SemiBold,
-    Syne_700Bold,
-  });
+  const resourcesLoaded = useCachedResources();
 
-  // useEffect(() => {
-  //   if (Platform.OS == "android") {
-  //     (async () => {
-  //       await NavigationBar.setButtonStyleAsync("dark");
-  //       await NavigationBar.setBackgroundColorAsync("#F8F9FA");
-  //     })();
-  //   }
-  // }, []);
-
-  if (!fontsLoaded) {
-    return <AppLoading />;
+  if (!resourcesLoaded) {
+    return null;
   }
 
   return (
-    <>
+    <SafeAreaProvider>
       <Navigation />
       <StatusBar />
-    </>
+    </SafeAreaProvider>
   );
 };
 
