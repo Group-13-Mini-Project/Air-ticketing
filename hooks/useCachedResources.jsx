7a632f15850a02ed26e5e4b465498b3966f0ca92
Joseph Ampadu <josephampadu549@gmail.com>
2022-05-08 17:55:28 +0000

    Upgrade expo sdk to 45. Bug fixes

---
+++ hooks/useCachedResources.jsx
@@ -0,0 +1,41 @@
+import {
+  Syne_400Regular,
+  Syne_500Medium,
+  Syne_600SemiBold,
+  Syne_700Bold,
+  useFonts,
+} from "@expo-google-fonts/syne";
+import { FontAwesome } from "@expo/vector-icons";
+import * as Font from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect, useState } from "react";
+
+const useCachedResources = () => {
+  const [resourcesLoaded, setResourcesLoaded] = useState(false);
+  useFonts({
+    Syne_400Regular,
+    Syne_500Medium,
+    Syne_600SemiBold,
+    Syne_700Bold,
+  });
+
+  useEffect(() => {
+    async function loadResourcesAsync() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        await Font.loadAsync({ ...FontAwesome.font });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setResourcesLoaded(true);
+        SplashScreen.hideAsync();
+      }
+    }
+
+    loadResourcesAsync();
+  }, []);
+
+  return resourcesLoaded;
+};
+
+export default useCachedResources;
